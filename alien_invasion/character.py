6efbbde136088681b098714d62d5d33bd908f539
Tashan <tashwri93@hotmail.co.uk>
2020-07-29 14:54:37 +0100

    Add files via upload

---
+++ alien_invasion/character.py
@@ -0,0 +1,15 @@
+import pygame
+
+class Character():
+    def __init__(self, screen):
+        self.screen = screen
+
+        self.image = pygame.image.load('images/robot.bmp')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.centery = self.screen_rect.centery
+
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
